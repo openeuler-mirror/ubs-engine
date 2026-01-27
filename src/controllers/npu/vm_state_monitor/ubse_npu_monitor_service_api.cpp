f66701e0d8748d63a753d5cc7fdb65bbf4922307
zhangkun2019 <zhangkun125@h-partners.com>
2026-01-27 19:43:52 +0800

    feat(npu):support npu reset

---
+++ src/controllers/npu/vm_state_monitor/ubse_npu_monitor_service_api.cpp
@@ -0,0 +1,191 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ * UbseEngine is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include "ubse_npu_monitor_service_api.h"
+
+#include <mutex>
+#include <optional>
+#include <securec.h>
+#include "ubse_error.h"
+#include "ubse_logger_inner.h"
+#include "ubse_npu_libvirt_monitor.h"
+#include "ubse_npu_resource_collection.h"
+#include "ubse_os_util.h"
+#include "ubse_pointer_process.h"
+#include "ubse_xml.h"
+
+namespace ubse::npu::vm_monitor {
+using namespace ubse::log;
+using namespace ubse::common::def;
+using namespace ubse::npu::controller;
+using namespace ubse::utils;
+UBSE_DEFINE_THIS_MODULE ("ubse", UBSE_CONTROLLER_MID);
+
+static LibvirtMonitor g_monitor("qemu::///system");
+
+void ResetNpu(uint8_t slotId, uint8_t chipId)
+{
+    constexpr char cmdTemplate[] =
+        "ipmitool raw 0x30 0x93 0xdb 0x07 0x00 0x8f 0x5c 0x00 0x00 0x80 0xff 0x%02x 0x%02x 0x00 0xc0 "
+        "0x00 0x00 0x00 0x01 0xff";
+    char realCmd[128]; // 128:数组长度
+    static_assert(sizeof(cmdTemplate) <= sizeof(realCmd) - 1);
+    if (sprintf_s(realCmd, sizeof(realCmd), cmdTemplate, slotId, chipId) == -1) {
+        UBSE_LOG_ERROR << "Failed to generate ipmi command.";
+        return;
+    }
+    std::string result;
+    UbseResult ret = UbseOsUtil::Exec(realCmd, result);
+    UBSE_LOG_INFO << "ipmi reset npu ret: " << ret << " ,output: " << result;
+}
+
+bool QueryAndReset(const std::string &busInstance)
+{
+    auto devPtr = ResourceCollection::GetInstance().GetDeviceByGuid(busInstance);
+    if (devPtr == nullptr) {
+        UBSE_LOG_ERROR << "Failed to get device resource:" << busInstance;
+        return false;
+    }
+    auto devBusiPtr = CollectionDevice::CollectionToDerived<CollectionDeviceBusi>(devPtr);
+    if (devBusiPtr == nullptr) {
+        UBSE_LOG_ERROR << "Failed to find bus instance: " << busInstance;
+        return false;
+    }
+    auto subIDevs = devBusiPtr->GetSubDevIdev();
+    for (auto &idev : subIDevs) {
+        if (idev == nullptr) {
+            continue;
+        }
+        auto david = idev->GetBondingDevDavid();
+        if (david == nullptr) {
+            continue;
+        }
+        auto loc = david->GetDeviceLoc();
+        ResetNpu(loc.slot_id, loc.chip_id);
+    }
+    return true;
+}
+
+std::string GetBusInstance(std::string_view xmlStr)
+{
+    std::string guid;
+    std::shared_ptr<UbseXml> ubseXml = SafeMakeShared<UbseXml>(xmlStr.data());
+    if (ubseXml == nullptr) {
+        UBSE_LOG_ERROR << "Failed to get ubse xml.";
+        return guid;
+    }
+    const auto ret = ubseXml->Parse();
+    if (ret != UbseXmlError::OK) {
+        UBSE_LOG_ERROR << "Failed to parse ubse xml.";
+        return guid;
+    }
+    ubseXml = ubseXml->Child("devices");
+    if (!ubseXml) {
+        UBSE_LOG_ERROR << "Failed to get devices.";
+        return guid;
+    }
+    int num = 0;
+    auto controller = ubseXml->Child("controller", num);
+    while (controller != nullptr) {
+        std::string type = controller->Attr("type");
+        if (type == "ub") {
+            ubseXml = ubseXml->Child("devices");
+            controller = ubseXml->Child("controller", num);
+            auto addressNode = controller->Child("address");
+            if (addressNode != nullptr) {
+                UBSE_LOG_ERROR << "Failed to get address node.";
+                return guid;
+            }
+            guid = addressNode->Attr("guid");
+            if (guid.empty()) {
+                UBSE_LOG_ERROR << "Failed to get guid node.";
+                return guid;
+            }
+            return guid;
+        }
+        ubseXml = ubseXml->Child("devices");
+        num++;
+        controller = ubseXml->Child("controller", num);
+    }
+    return guid;
+}
+
+static void VmStateCallback(std::string_view name, VmEventType event, const std::shared_ptr<char> &xmlPtr)
+{
+    if (xmlPtr == nullptr) {
+        UBSE_LOG_ERROR << "xmlPtr is nullptr.";
+        return;
+    }
+    auto guid_str = GetBusInstance(xmlPtr.get());
+    if (guid_str.empty()) {
+        UBSE_LOG_ERROR << "guid is empty";
+        return;
+    }
+    guid_str.erase(std::remove_if(guid_str.begin(), guid_str.end(), [](char c) { return c == '-'; }), guid_str.end());
+    ResetNpuOfBusInstance(guid_str, event);
+}
+
+UbseResult StartVMMonitor()
+{
+    g_monitor.SetCallBack(VmStateCallback);
+    if (!g_monitor.Start()) {
+        UBSE_LOG_ERROR << "Failed to start VM monitor.";
+        return UBSE_ERROR;
+    }
+    UBSE_LOG_INFO << "Libvirt monitor running";
+    return UBSE_OK;
+}
+
+void ResetNpuOfBusInstance(const std::string &busInstance, VmEventType event)
+{
+    static std::unordered_map<std::string, VmEventType> record;
+    static std::mutex recordMutex;
+    bool needReset = false;
+    std::lock_guard<std::mutex> lock(recordMutex);
+    if (const auto it = record.find(busInstance); it != record.end()) {
+        if (it->second == event) {
+            return;
+        }
+        switch (event) {
+            case VmEventType::DESTROY: {
+                const VmEventType oldEvent = it->second;
+                record.erase(it);
+                needReset = (oldEvent != VmEventType::STOPPED);
+                break;
+            }
+            case VmEventType::CREATE: {
+                it->second = event;
+                needReset = true;
+                break;
+            }
+            case VmEventType::STARTED: {
+                needReset = (it->second != VmEventType::CREATE);
+                it->second = event;
+                break;
+            }
+            case VmEventType::STOPPED: {
+                it->second = event;
+                needReset = true;
+                break;
+            }
+            default:
+                break;
+        }
+    } else {
+        record[busInstance] = event;
+        needReset = true;
+    }
+    if (needReset) {
+        QueryAndReset(busInstance);
+    }
+}
+}
---
+++ src/controllers/npu/vm_state_monitor/ubse_npu_monitor_service_api.h
@@ -0,0 +1,24 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ * UbseEngine is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#ifndef UBSE_MONIOR_SERVICE_API_H
+#define UBSE_MONIOR_SERVICE_API_H
+
+#include "ubse_common_def.h"
+#include "ubse_npu_monitor_def.h"
+
+namespace  ubse::npu::vm_monitor {
+ubse::common::def::UbseResult StartVMMonitor();
+
+void ResetNpuOfBusInstance(const std::string &busInstance, VmEventType event);
+}
+#endif // UBSE_MONIOR_SERVICE_API_H
