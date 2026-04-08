a4513f09c579cda568fe16509c6dc45f79dbe449
zhang_qin_5 <zhangqin99@h-partners.com>
2026-04-08 20:37:04 +0800

    fix: Add script dependencies

--- scripts/rpm/package.sh
+++ scripts/rpm/package.sh
@@ -7,13 +7,15 @@ SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PROJECT_DIR=$(dirname "$(dirname "$SCRIPTS_DIR")")
 PROJECT_BASENAME="$(basename "$PROJECT_DIR")"
 PACKAGE_NAME="ubs-engine"
+SOURCE_PACKAGE="ubs-engine.tar.gz"
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 cd "$(dirname "$PROJECT_DIR")"
-tar -czf $PACKAGE_NAME.tar.gz \
+tar -czf $SOURCE_PACKAGE \
     --exclude="$PROJECT_BASENAME/cmake-build-*" \
     "$PROJECT_BASENAME"
-mv $PACKAGE_NAME.tar.gz ~/rpmbuild/SOURCES/
+mv $SOURCE_PACKAGE ~/rpmbuild/SOURCES/
 cp "$PROJECT_DIR"/$PACKAGE_NAME.spec ~/rpmbuild/SPECS/
+sed -i "s/%{name}-%{version}.tar.gz/$SOURCE_PACKAGE/" ~/rpmbuild/SPECS/$PACKAGE_NAME.spec
 sed -i "s/%define project_dir %{name}-%{version}/%define project_dir $PROJECT_BASENAME/" ~/rpmbuild/SPECS/$PACKAGE_NAME.spec
 sed -i "s/BuildRequires/#BuildRequires/" ~/rpmbuild/SPECS/$PACKAGE_NAME.spec
 if [[ "$1" == '-D' ]]; then
