e7c6e75ab20907880b5406e87175adb7a93fd3f5
q00956987 <quanletian@huawei.com>
2026-04-09 11:12:59 +0800

    test

--- src/controllers/node/ubse_node_controller_master.h
+++ src/controllers/node/ubse_node_controller_master.h
@@ -85,10 +85,7 @@ private:
 
     void UbseNodeRetryLedger(const std::string& nodeId);
 
-    /**
-     * 节点上报汇聚，每隔1min，打印一次节点上报记录
-     */
-    void ReportAggregation();
+    UbseResult ReportAggregationTimerHandler();
 
     UbseTaskExecutorPtr taskExecutor_{};
 
