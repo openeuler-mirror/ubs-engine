9db671e570760fc288489f634d162753313c119b
davidhwang <huangdewei@huawei.com>
2026-04-21 19:13:11 +0800

    fix bug

--- src/addons/rmrs/over_commit/fault_management/over_commit_fault_node_module.cpp
+++ src/addons/rmrs/over_commit/fault_management/over_commit_fault_node_module.cpp
@@ -789,7 +789,7 @@ MpResult OverCommitFaultNodeModule::BorrowInNodeProcess(const FaultRecordsInNode
         remoteNumaId2RecordMap[record.borrowRemoteNuma].push_back(record);
         remoteNumaId2LocalNumaId[record.borrowRemoteNuma].insert(record.borrowLocalNuma);
     }
-    auto res = MEM_POOLING_OK;
+    MpResult res = MEM_POOLING_OK;
     // 2. 遍历每个远端numa，获取与该远端numa相关的虚机信息
     for (auto &remoteNumaPair : remoteNumaId2RecordMap) {
         auto ret = ProcessSingleFaultRemoteNuma(remoteNumaPair, remoteNumaId2LocalNumaId);
@@ -797,12 +797,12 @@ MpResult OverCommitFaultNodeModule::BorrowInNodeProcess(const FaultRecordsInNode
             LOG_ERROR << "ProcessSingleFaultRemoteNuma failed, remoteNumaId=" << remoteNumaPair.first
                       << ", ret=" << ret;
             // 故障场景下继续处理
+            res = MEM_POOLING_ERROR;
         }
         MpResult retBorrowIdInFaultProcess = BorrowIdInFaultProcess::Instance().Clear();
         if (retBorrowIdInFaultProcess != MEM_POOLING_OK) {
             UBSE_LOGGER_ERROR(MP_MODULE_NAME, MP_MODULE_CODE)
                 << "[MemReturn] Clear fault process borrowId failed. ret=" << retBorrowIdInFaultProcess << ".";
-            res = MEM_POOLING_ERROR;
         }
     }
     LOG_DEBUG << "BorrowInNodeProcess end.";
