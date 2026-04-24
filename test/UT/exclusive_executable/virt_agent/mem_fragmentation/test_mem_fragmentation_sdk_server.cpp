9a329484cef12864caf7e4295fe6e1fb828d66b9
xyhxou <3401936276@qq.com>
2026-04-24 10:38:14 +0800

    [virtagent] #fix # UT修复

--- test/UT/exclusive_executable/virt_agent/mem_fragmentation/test_mem_fragmentation_sdk_server.cpp
+++ test/UT/exclusive_executable/virt_agent/mem_fragmentation/test_mem_fragmentation_sdk_server.cpp
@@ -16,6 +16,7 @@
 #include <mockcpp/mockcpp.hpp>
 #include <ubse_api_server.h>
 #include <ubse_election.h>
+#include <ubse_security.h>
 
 #include "mem_fragmentation_sdk_server.h"
 #include "mem_fragmentation_msg.h"
@@ -29,6 +30,7 @@
 
 using namespace api::server;
 using namespace ubse::election;
+using namespace ubse::security;
 using namespace vm;
 using namespace vm::mempooling;
 namespace ubse::ut::vm {
@@ -854,6 +856,7 @@ TEST_F(TestMemFragmentationSdkServer, SetSrcNodeHugePage_ShouldReturnError_WhenS
 {
     MemBorrowExecuteResult result{};
     result.presentNumaIds.push_back(1);
+    MOCKER(ChangeOverrideCapability).stubs().will(returnValue(VM_OK));
     MOCKER(MemHandler::GetBorrowedSizeMap).stubs().will(invoke(MockGetBorrowedSizeMap));
     MOCKER(HugePageHandler::SetHugePages).stubs().will(returnValue(VM_ERROR_NOMEM));
     EXPECT_EQ(VirtMemFragSdk::SetSrcNodeHugePage(result), VM_ERROR_NOMEM);
@@ -870,6 +873,7 @@ TEST_F(TestMemFragmentationSdkServer, SetSrcNodeHugePage_ShouldReturnOK_WhenEver
 {
     MemBorrowExecuteResult result{};
     result.presentNumaIds.push_back(1);
+    MOCKER(ChangeOverrideCapability).stubs().will(returnValue(VM_OK));
     MOCKER(MemHandler::GetBorrowedSizeMap).stubs().will(invoke(MockGetBorrowedSizeMap));
     MOCKER(HugePageHandler::SetHugePages).stubs().will(returnValue(VM_OK));
     EXPECT_EQ(VirtMemFragSdk::SetSrcNodeHugePage(result), VM_OK);
