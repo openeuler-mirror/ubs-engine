0877e864dce8d2bd79e972b5c85e6a153e0dd563
subceux <liujiangqi1@huawei.com>
2026-04-28 20:05:47 +0800

    rmrs ut update

--- test/UT/exclusive_executable/rmrs/over_commit/fault_management/test_over_commit_fault_management_handler.cpp
+++ test/UT/exclusive_executable/rmrs/over_commit/fault_management/test_over_commit_fault_management_handler.cpp
@@ -19,6 +19,8 @@
 #include "over_commit_fault_memid_helper.h"
 #include "over_commit_fault_memid_module.h"
 #include "over_commit_fault_node_module.h"
+#include "mem_borrow_executor.h"
+#include "mp_smap_helper.h"
 #define MOCKER_CPP(api, TT) MOCKCPP_NS::mockAPI<>::get(#api, "", api)
 
 namespace mempooling::over_commit {
@@ -125,6 +127,244 @@ TEST_F(TestOverCommitFaultManagementHandler, MemIdExecuteResHandler_Failed)
     uint32_t resCode = MEM_POOLING_ERROR;
     OverCommitFaultManagementHandler::MemIdExecuteResHandler(&ctx, resp, resCode);
     EXPECT_EQ(ctx, MEM_POOLING_ERROR);
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnExecuteResHandler_Succeed)
+{
+    uint32_t ctx = 0;
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::MemIdReturnExecuteResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnExecuteResHandler_Failed)
+{
+    uint32_t ctx = 1;
+    UbseByteBuffer resp;
+    resp.len = 1;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_ERROR;
+    OverCommitFaultManagementHandler::MemIdReturnExecuteResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_ERROR);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnExecuteResHandler_CtxNull)
+{
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::MemIdReturnExecuteResHandler(nullptr, resp, resCode);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnDirectlyExecuteResHandler_Succeed)
+{
+    uint32_t ctx = 0;
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::MemIdReturnDirectlyExecuteResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnDirectlyExecuteResHandler_Failed)
+{
+    uint32_t ctx = 1;
+    UbseByteBuffer resp;
+    resp.len = 1;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_ERROR;
+    OverCommitFaultManagementHandler::MemIdReturnDirectlyExecuteResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_ERROR);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnDirectlyExecuteResHandler_CtxNull)
+{
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::MemIdReturnDirectlyExecuteResHandler(nullptr, resp, resCode);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, DisableSmapProcessMigrateResHandler_Succeed)
+{
+    uint32_t ctx = 0;
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::DisableSmapProcessMigrateResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, DisableSmapProcessMigrateResHandler_Failed)
+{
+    uint32_t ctx = 1;
+    UbseByteBuffer resp;
+    resp.len = 1;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_ERROR;
+    OverCommitFaultManagementHandler::DisableSmapProcessMigrateResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_ERROR);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, DisableSmapProcessMigrateResHandler_CtxNull)
+{
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::DisableSmapProcessMigrateResHandler(nullptr, resp, resCode);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, FaultNumaProcessResHandler_Succeed)
+{
+    uint32_t ctx = 0;
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::FaultNumaProcessResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, FaultNumaProcessResHandler_Failed)
+{
+    uint32_t ctx = 1;
+    UbseByteBuffer resp;
+    resp.len = 1;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_ERROR;
+    OverCommitFaultManagementHandler::FaultNumaProcessResHandler(&ctx, resp, resCode);
+    EXPECT_EQ(ctx, MEM_POOLING_ERROR);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, FaultNumaProcessResHandler_CtxNull)
+{
+    UbseByteBuffer resp;
+    resp.len = MEM_POOLING_ERROR;
+    resp.data = new uint8_t[1];
+    uint32_t resCode = MEM_POOLING_OK;
+    OverCommitFaultManagementHandler::FaultNumaProcessResHandler(nullptr, resp, resCode);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnExecuteRecvHandler_Succeed)
+{
+    MOCKER_CPP(&MemBorrowExecutor::MemFreeWithOps,
+               MpResult(*)(MemBorrowExecutor *, const std::string &, bool, bool, bool))
+        .stubs()
+        .will(returnValue(MEM_POOLING_OK));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::MemIdReturnExecuteRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnExecuteRecvHandler_MemFreeWithOps_Failed)
+{
+    MOCKER_CPP(&MemBorrowExecutor::MemFreeWithOps,
+               MpResult(*)(MemBorrowExecutor *, const std::string &, bool, bool, bool))
+        .stubs()
+        .will(returnValue(MEM_POOLING_ERROR));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::MemIdReturnExecuteRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_ERROR);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnDirectlyExecuteRecvHandler_Succeed)
+{
+    MOCKER_CPP(&MemBorrowExecutor::MemFreeWithOps,
+               MpResult(*)(MemBorrowExecutor *, const std::string &, bool, bool, bool))
+        .stubs()
+        .will(returnValue(MEM_POOLING_OK));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::MemIdReturnDirectlyExecuteRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, MemIdReturnDirectlyExecuteRecvHandler_MemFreeWithOps_Failed)
+{
+    MOCKER_CPP(&MemBorrowExecutor::MemFreeWithOps,
+               MpResult(*)(MemBorrowExecutor *, const std::string &, bool, bool, bool))
+        .stubs()
+        .will(returnValue(MEM_POOLING_ERROR));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::MemIdReturnDirectlyExecuteRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_ERROR);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, DisableSmapProcessMigrateRecvHandler_Succeed)
+{
+    MOCKER_CPP(&MpSmapHelper::SmapEnableProcessMigrateHelper, int(*)(pid_t *, size_t, int, int))
+        .stubs()
+        .will(returnValue(MEM_POOLING_OK));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::DisableSmapProcessMigrateRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, DisableSmapProcessMigrateRecvHandler_SmapHelper_Failed)
+{
+    MOCKER_CPP(&MpSmapHelper::SmapEnableProcessMigrateHelper, int(*)(pid_t *, size_t, int, int))
+        .stubs()
+        .will(returnValue(MEM_POOLING_ERROR));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::DisableSmapProcessMigrateRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_ERROR);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, FaultNumaProcessRecvHandler_Succeed)
+{
+    MOCKER_CPP(&OverCommitFaultNodeModule::BorrowInNodeProcess,
+               MpResult(*)(OverCommitFaultNodeModule *, const FaultRecordsInNode &))
+        .stubs()
+        .will(returnValue(MEM_POOLING_OK));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::FaultNumaProcessRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_OK);
+    delete[] resp.data;
+}
+
+TEST_F(TestOverCommitFaultManagementHandler, FaultNumaProcessRecvHandler_BorrowInNodeProcess_Failed)
+{
+    MOCKER_CPP(&OverCommitFaultNodeModule::BorrowInNodeProcess,
+               MpResult(*)(OverCommitFaultNodeModule *, const FaultRecordsInNode &))
+        .stubs()
+        .will(returnValue(MEM_POOLING_ERROR));
+    UbseByteBuffer req;
+    UbseByteBuffer resp;
+    uint32_t ret = OverCommitFaultManagementHandler::FaultNumaProcessRecvHandler(req, resp);
+    EXPECT_EQ(ret, MEM_POOLING_ERROR);
+    delete[] resp.data;
 }
 
 } // namespace mempooling::over_commit
