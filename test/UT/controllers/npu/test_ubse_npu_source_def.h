4ac731470cc961930d4bee57a68cbfc14e202895
zhangkun2019 <zhangkun125@h-partners.com>
2026-07-07 09:35:02 +0800

    [ubse]补充ubse_npu_source_def.cpp文件代码行覆盖率达到70%

---
+++ test/UT/controllers/npu/test_ubse_npu_source_def.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ * ubs-engine is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#ifndef TEST_UBSE_NPU_SOURCE_DEF_H
+#define TEST_UBSE_NPU_SOURCE_DEF_H
+
+#include <gtest/gtest.h>
+#include <mockcpp/mockcpp.hpp>
+#include "ubse_npu_source_def.h"
+
+namespace ubse::npu::controller::ut {
+
+class TestUbseNpuSourceDef : public testing::Test {
+public:
+    TestUbseNpuSourceDef() = default;
+    void SetUp() override;
+    void TearDown() override;
+};
+
+} // namespace ubse::npu::controller::ut
+
+#endif // TEST_UBSE_NPU_SOURCE_DEF_H
