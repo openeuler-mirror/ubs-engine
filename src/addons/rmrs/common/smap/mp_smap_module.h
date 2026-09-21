26c0d1b40b41ea1d20dd1c94c9625e98a0d857e3
davidhwang <huangdewei@huawei.com>
2026-09-21 18:18:30 +0800

    fix(RMRS): 修正smap结构体本地NUMA组数限制
    - 将本地NUMA组最大数量从4增加到8
    - 修改对应的宏定义以支持更多本地NUMA组
    - 保持迁出接口及其他相关逻辑兼容性

--- src/addons/rmrs/common/smap/mp_smap_module.h
+++ src/addons/rmrs/common/smap/mp_smap_module.h
@@ -37,7 +37,7 @@ constexpr uint16_t FAIL_RETRY_COUNT = 10;
 const int MAX_NR_MIGOUT = 40;
 constexpr int MAX_NR_GROUPED_MIGOUT = MAX_NR_MIGOUT;
 constexpr int MAX_MIGRATION_GROUP_NUM = 8;
-constexpr int MAX_GROUP_LOCAL_NUMA = 4;
+constexpr int MAX_GROUP_LOCAL_NUMA = 8;
 constexpr int MAX_GROUP_REMOTE_NUMA = REMOTE_NUMA_NUM;
 
 // 迁出接口超时返回码
